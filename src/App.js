4046c5a590f92044bdbc289f437676f833ade495
Vishwak Kotha <vishwakkotha@gmail.com>
2024-05-24 10:27:38 +0530

    weatherappreact

--- src/App.js
+++ src/App.js
@@ -9,17 +9,15 @@ function App() {
         <CurrentLocation />
       </div>
       <div className="footer-info">
-        <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
+        <a href="#">
           Download Source Code
         </a>{" "}
         | Developed by{" "}
-        <a target="_blank" href="https://www.gauravghai.dev/">
-          Gaurav Ghai
+        <a target="_blank" href="#">
+          Kotha vishwak
         </a>{" "}
-        | Powered by{" "}
-        <a target="_blank" href="https://www.htmlhints.com/">
-          HTML HINTS
-        </a>
+        
+        
       </div>
     </React.Fragment>
   );
--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "d3776be56b42d5cfd83b7eaa5feafaff",
   base: "https://api.openweathermap.org/data/2.5/",
 };
